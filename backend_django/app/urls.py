25f3bdbc9cff89ea49f82462bd1cd3f5c0faf7e4
webapp-master <ogundirantoluwani@gmail.com>
2023-10-04 19:43:23 +0100

    I am encountering a problem with the 'getProduct' view.function

--- backend_django/app/urls.py
+++ backend_django/app/urls.py
@@ -5,5 +5,6 @@ from app import views
 urlpatterns = [
     path('',views.getRoutes,name="getRoutes"),
     path('products/',views.getProducts,name="getProducts"),
+    path('products/<str:pk>',views.getProduct,name="getProduct"),
 
 ]
--- backend_django/app/views.py
+++ backend_django/app/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello',safe=False)
+    return Response('Hello')
 
+
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products,safe=False)
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product=None
+    for i in products:
+        if i['_id']==pk:
+            product=i
+            break
+        return Response(product)
 
 
